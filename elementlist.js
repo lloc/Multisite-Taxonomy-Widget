c704643c986562beb61a2494d26c6d8cfbcaad93
Dennis Ploetner <re@lloc.de>
2013-02-25 10:12:51 +0100

    Documentation 0.7

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","mtw_cmp_posts()"],["f","mtw_create_shortcode()"],["f","mtw_get_formatelements()"],["f","mtw_get_posts()"],["f","mtw_get_posts_from_blogs()"],["f","mtw_get_thumbnail()"],["f","mtw_plugin_init()"],["c","MultisiteTaxonomyWidget"]];
+ApiGen.elements = [["f","mtw_cmp_posts()"],["f","mtw_create_shortcode()"],["f","mtw_get_formatelements()"],["f","mtw_get_posts()"],["f","mtw_get_posts_from_blogs()"],["f","mtw_get_thumbnail()"],["f","mtw_plugin_init()"],["f","mtw_post_faxctory()"],["c","MultisiteTaxonomyWidget"],["c","WP_Post"]];
